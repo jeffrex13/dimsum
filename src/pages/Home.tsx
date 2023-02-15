8065683995bc9cfbe92ccb7b07dd7dd5dadb1676
Jeffrey <jepcureg@gmail.com>
2023-02-15 14:13:54 +0800

    creating navbar component and adding poppins font

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 const Home = () => {
-  return <div className=" bg-[#FFB21B] h-screen">home</div>
+  return <div className="bg-[#FFB21B] h-screen">home</div>
 }
 
 export default Home
