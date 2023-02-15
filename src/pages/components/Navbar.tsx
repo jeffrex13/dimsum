c43607a7e83aa15aab2434e4d5653bc32cb80f3e
Jeffrey <jepcureg@gmail.com>
2023-02-15 16:22:48 +0800

    adding image and text to home

--- src/pages/components/Navbar.tsx
+++ src/pages/components/Navbar.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className="bg-[#FFB21B] px-24 py-8 flex justify-between">
+    <div className="bg-[#FFB21B] px-40 py-8 flex items-center justify-between">
       <div>
-        <p className=" font-semibold text-xl">Sample Company</p>
+        <p className=" font-semibold text-xl text-[#421C13]">Sample Company</p>
       </div>
       <div>
         <ul className="flex gap-16 text-s">
